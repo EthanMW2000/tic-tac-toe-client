114ba236fdd43cb70c591f28d88b4787a572f388
Ethan Wells <ethanwells@nuvative.com>
2023-05-04 15:26:10 -0500

    added adjacency calculations

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -1,47 +1,50 @@
 import { useState } from "react";
-import { IBoard, Player } from "../types";
+import { IBoard, ISlot, Player } from "../types";
 import Slot from "./slot";
 
 export default function Board(props: IBoard) {
   const [player, setPlayer] = useState<Player>(Player.Circle);
+  const [playedSlots, setPlayedSlots] = useState<ISlot[]>([]);
 
   function updatePlayer(newPlayer: Player) {
     setPlayer(newPlayer);
-  };
+  }
 
   function SetupBoard() {
-    let col: number[] = []
-    for(let i = 0; i < (props.rowsColumns); i++) {
-      col.push(i)
+    let col: number[] = [];
+    for (let i = 0; i < props.rowsColumns; i++) {
+      col.push(i);
     }
 
-    let rows: number[] = []
-    for(let i = 0; i < (props.rowsColumns); i++) {
-      rows.push(i)
+    let rows: number[] = [];
+    for (let i = 0; i < props.rowsColumns; i++) {
+      rows.push(i);
     }
-
-    console.log(rows.length)
 
     return rows.map((row) => {
       return (
-        <div key={row} className="h-20 md:h-32 lg:h-40 flex flex-row justify-center">
-          {
-            col.map((slot) => {
-              return (
-                <Slot
-                  id={slot + row * props.rowsColumns}
-                  key={slot + row * props.rowsColumns}
-                  updatePlayer={updatePlayer}
-                  row={row}
-                  col={slot}
-                  assignment={player}
-                />
-              );
-            })
-          }
+        <div
+          key={row}
+          className="h-20 md:h-32 lg:h-40 flex flex-row justify-center"
+        >
+          {col.map((slot) => {
+            return (
+              <Slot
+                id={slot + row * props.rowsColumns}
+                key={slot + row * props.rowsColumns}
+                updatePlayer={updatePlayer}
+                setPlayedSlots={setPlayedSlots}
+                playedSlots={playedSlots}
+                rowsColumns={props.rowsColumns}
+                row={row}
+                col={slot}
+                assignment={player}
+              />
+            );
+          })}
         </div>
-      )
-    })
+      );
+    });
   }
 
   return (
